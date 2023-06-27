29531dd06e5972539d67c2145994666553d57ffd
Jordan Fontaine <jordanmfontaine@gmail.com>
2023-06-27 00:45:53 -0500

    created config folder with planet file to manage specific data configs

---
+++ out-of-this-world/config/planets.ts
@@ -0,0 +1,68 @@
+import { PlanetProps } from "@types";
+
+export const planetProps: PlanetProps[] = [
+  // Photo by ivan Torres on Unsplash
+  {
+    id: "c",
+    title: "Pizza",
+    description: "5 Food Apps Delivering the Best of Your City",
+    distance: 80,
+    transportMethod: "#814A0E",
+  },
+  // Photo by Dennis Brendel on Unsplash
+  {
+    id: "f",
+    title: "How to",
+    description: "Arrange Your Apple Devices for the Gram",
+    distance: 120,
+    transportMethod: "#959684",
+  },
+  // Photo by Alessandra Caretto on Unsplash
+  {
+    id: "a",
+    title: "Pedal Power",
+    description: "Map Apps for the Superior Mode of Transport",
+    distance: 260,
+    transportMethod: "#5DBCD2",
+  },
+  // Photo by Taneli Lahtinen on Unsplash
+  {
+    id: "g",
+    title: "Holidays",
+    description: "Our Pick of Apps to Help You Escape From Apps",
+    distance: 200,
+    transportMethod: "#8F986D",
+  },
+  // Photo by Simone Hutsch on Unsplash
+  {
+    id: "d",
+    title: "Photography",
+    description: "The Latest Ultra-Specific Photography Editing Apps",
+    distance: 150,
+    transportMethod: "#FA6779",
+  },
+  // Photo by Siora Photography on Unsplash
+  {
+    id: "h",
+    title: "They're all the same",
+    description: "100 Cupcake Apps for the Cupcake Connoisseur",
+    distance: 60,
+    transportMethod: "#282F49",
+  },
+  // Photo by Yerlin Matu on Unsplash
+  {
+    id: "e",
+    title: "Cats",
+    description: "Yes, They Are Sociopaths",
+    distance: 200,
+    transportMethod: "#AC7441",
+  },
+  // Photo by Ali Abdul Rahman on Unsplash
+  {
+    id: "b",
+    title: "Holidays",
+    description: "Seriously the Only Escape is the Stratosphere",
+    distance: 260,
+    transportMethod: "#CC555B",
+  },
+];
