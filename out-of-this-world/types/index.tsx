b72bffa059957055e7e8993b323c2f63e38e30b3
Jordan Fontaine <jordanmfontaine@gmail.com>
2023-06-28 14:24:11 -0500

    updated types

--- out-of-this-world/types/index.tsx
+++ out-of-this-world/types/index.tsx
@@ -1,11 +1,34 @@
 import { MouseEventHandler } from "react";
 
+// The props within this types folder are mainly for major components.
+// The props for the items HELD WITHIN these components are located in the config file
+
 export interface CustomButtonProps {
-  isDisabled?: boolean;
-  btnType?: "button" | "submit";
-  containerStyles?: string;
-  textStyles?: string;
-  title: string;
-  rightIcon?: string;
+  text: string;
   handleClick?: MouseEventHandler<HTMLButtonElement>;
+}
+
+export interface AnimatedTextProps {
+  text: string;
+  className?: string;
+}
+
+export interface PlanetDetailsProps {
+  id: string;
+  name: string;
+  distance: number;
+  description: string;
+  transportMethod: string;
+}
+export interface PlanetCardProps {
+  id: string;
+  title: string;
+  description?: string;
+}
+
+export interface TransportDetails {
+  id: string;
+  title: string;
+  maxSpeed: string;
+  description: string;
 }
