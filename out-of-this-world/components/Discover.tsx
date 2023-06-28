5c0bf77460e1964548f7876572ff262451a1ce03
Jordan Fontaine <jordanmfontaine@gmail.com>
2023-06-28 14:17:44 -0500

    removing commented code

--- out-of-this-world/components/Discover.tsx
+++ out-of-this-world/components/Discover.tsx
@@ -8,25 +8,17 @@ type Props = {};
 
 const Discover = (props: Props) => {
   return (
-    // <div className="w-full h-auto pt-12">
-    //   <header className="border-b-stone-200 border-solid border-b-2">
-    //     <span className="text-stone-200 text-sm uppercase">
-    //       Tuesday, June 26th{" "}
-    //     </span>
-    //     <h1 className="text-bold text-white text-xl mt-2 mb-3">Today</h1>
-    //   </header>
-
-    //   <ul>
-    //     {planetProps.map((planet) => (
-    //       <PlanetCard planet={planet} />
-    //     ))}
-    //   </ul>
-    // </div>
-    <div className="px-36 py-20">
-      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 mx-5 h-full gap-x-6 gap-y-10">
-        {planetProps.map((planet) => (
-          <PlanetCard planet={planet} />
-        ))}
+    <div className="min-h-screen flex flex-col">
+      <header>
+        <h1 className="text-white">Currently Scheduled Flights</h1>
+        <p>current date</p>
+      </header>
+      <div className="px-36 py-36 flex flex-col items-center justify-center">
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 mx-5 h-full gap-x-6 gap-y-10">
+          {planetProps.map((planet) => (
+            <PlanetCard planet={planet} />
+          ))}
+        </div>
       </div>
     </div>
   );
