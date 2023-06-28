a4c861f808cf2fd820eec1071182aa4c91d78e4e
Jordan Fontaine <jordanmfontaine@gmail.com>
2023-06-28 04:49:59 -0500

    refactored the hero component. Removed logic

--- out-of-this-world/components/Hero.tsx
+++ out-of-this-world/components/Hero.tsx
@@ -1,7 +1,4 @@
 "use client";
-import Image from "next/image";
-import ship2 from "../public/ships/ship-2.png";
-import CustomButton from "./CustomButton";
 
 const Hero = () => {
   const handleScroll = () => {
@@ -13,29 +10,10 @@ const Hero = () => {
   };
 
   return (
-    <div className="min-h-screen w-full grid grid-cols-3 bg-black">
-      {/* LEFT */}
-      <div className="col-span-1 flex flex-col items-center justify-center w-full">
-        <h1 className="text-4xl text-white font-semibold">
-          Where are you headed?
-        </h1>
-
-        <p className="text-white">
-          Streamline your space travel experience with our effortless booking
-          process.
-        </p>
-
-        <CustomButton
-          title="Explore Destinations"
-          containerStyles="bg-primary-blue text-white px-20 rounded-full mt-10"
-          handleClick={handleScroll}
-        />
-      </div>
-
-      {/* RIGHT */}
-      <div className="col-span-2 w-full min-h-screen bg-right bg-no-repeat bg-[url('../public/ships/ship-1.png')]">
-        {/* <div className="mx-30 right-0 w-1/4 h-full backdrop-brightness-50"></div> */}
-      </div>
+    <div className="min-h-screen w-full flex flex-col">
+      <div className="w-full min-h-screen bg-cover bg-center bg-no-repeat bg-[url('../public/ships/ship-4.png')]" />
+      
+      
     </div>
   );
 };
