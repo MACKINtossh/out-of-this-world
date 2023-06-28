0146af71f16bceba25b60603dec32d70668d30fe
Jordan Fontaine <jordanmfontaine@gmail.com>
2023-06-28 04:52:45 -0500

    mapped grid cards to Discover page

--- out-of-this-world/components/Discover.tsx
+++ out-of-this-world/components/Discover.tsx
@@ -8,19 +8,26 @@ type Props = {};
 
 const Discover = (props: Props) => {
   return (
-    <div className="w-full h-auto pt-12">
-      <header className="border-b-stone-200 border-solid border-b-2">
-        <span className="text-stone-200 text-sm uppercase">
-          Tuesday, June 26th{" "}
-        </span>
-        <h1 className="text-bold text-white text-xl mt-2 mb-3">Today</h1>
-      </header>
+    // <div className="w-full h-auto pt-12">
+    //   <header className="border-b-stone-200 border-solid border-b-2">
+    //     <span className="text-stone-200 text-sm uppercase">
+    //       Tuesday, June 26th{" "}
+    //     </span>
+    //     <h1 className="text-bold text-white text-xl mt-2 mb-3">Today</h1>
+    //   </header>
 
-      <ul>
+    //   <ul>
+    //     {planetProps.map((planet) => (
+    //       <PlanetCard planet={planet} />
+    //     ))}
+    //   </ul>
+    // </div>
+    <div className="px-36 py-20">
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 mx-5 h-full gap-x-6 gap-y-10">
         {planetProps.map((planet) => (
           <PlanetCard planet={planet} />
         ))}
-      </ul>
+      </div>
     </div>
   );
 };
