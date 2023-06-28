4121242405a094d41042d76ecfcb30b2da15d3d1
Jordan Fontaine <jordanmfontaine@gmail.com>
2023-06-28 14:25:52 -0500

    imported PlanetCard component

--- out-of-this-world/components/index.ts
+++ out-of-this-world/components/index.ts
@@ -2,6 +2,7 @@ import CustomButton from "./CustomButton";
 import Footer from "./Footer";
 import Hero from "./Hero";
 import Navbar from "./Navbar";
+import Discover from "./Discover";
+import PlanetCard from "./ui/PlanetCard";
 
-
-export { Hero, CustomButton, Navbar, Footer };
+export { Hero, CustomButton, Navbar, Discover, PlanetCard, Footer };
